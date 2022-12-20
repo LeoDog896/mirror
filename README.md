7a2454d2eaf31b30c869e01b945bfa48a8ca411b
obama <LeoDog896@hotmail.com>
2022-12-20 21:15:26 +0000

    mirror option

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mirror
 
 https://leodog896.github.io/mirror
+
+To mirror it, add: https://leodog896.github.io/mirror#mirror
--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 const video = document.querySelector<HTMLVideoElement>("#videoElement")!;
+const hash = location.hash.substring(1)
+
+if (hash === "mirror") {
+    video.style.transform = "scaleX(-1)"
+} else {
+    video.style.transform = "scaleX(1)"
+}
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
